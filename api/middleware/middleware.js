74484cf73d0c54c0ac618f27682f94e42ee54bfa
Weston Woodard <westonwoodard28@gmail.com>
2021-04-07 16:44:00 -0700

    MVP complete

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,67 @@
+const Users = require('../users/users-model.js');
+
+
+// logger()
+// logger logs to the console the following information about each request: request method, request url, and a timestamp
+// this middleware runs on every request made to the API
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+    'Origin'
+  )}`)
+    next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+
+// validateUserId()
+// this middleware will be used for all user endpoints that include an id parameter in the url (ex: /api/users/:id and it should check the database to make sure there is a user with that id.
+// if the id parameter is valid, store the user object as req.user and allow the request to continue
+// if the id parameter does not match any user id in the database, respond with status 404 and { message: "user not found" }
+const validateUserId = async (req, res, next) => {
+  const id = req.params.id
+  try{
+    const user = await Users.getById(id)
+    if(!user){
+      res.status(404).json({message:`user not found`})
+    }else{
+      req.user = user
+      next()
+    }
+  }catch(e){
+    res.status(500).json(e.message)
+  }
 }
 
+
+// validateUser()
+// validateUser validates the body on a request to create or update a user
+// if the request body lacks the required name field, respond with status 400 and { message: "missing required name field" }
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if(!req.body.name || Object.keys(req.body.name).length === 0){
+    res.status(400).json({message: `missing required name field`})
+  }else{
+    next()
+  }
 }
 
+
+// validatePost()
+// validatePost validates the body on a request to create a new post
+// if the request body lacks the required text field, respond with status 400 and { message: "missing required text field" }
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if(!req.body.text || Object.keys(req.body.text).length === 0){
+    res.status(400).json({message: `missing required text field`})
+  }else{
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
+
+
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const usersRouter = require('./users/users-router.js');
+const mw = require('./middleware/middleware.js');
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
-
-// global middlewares and the user's router need to be connected here
+server.use(express.json());
+server.use(mw.logger);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
